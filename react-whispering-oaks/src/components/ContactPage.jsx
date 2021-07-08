cd7d8ee211938dfed4b9f71d65681cd421e31ec5
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-07-08 14:36:02 -0500

    added trailing slash

--- react-whispering-oaks/src/components/ContactPage.jsx
+++ react-whispering-oaks/src/components/ContactPage.jsx
@@ -8,7 +8,7 @@ import './contact.css'
 
 // let url = 'http://localhost:5000/api/v1/whispering_oaks/'
 
-let url = 'https://whispering-oaks.herokuapp.com/api/v1/whispering_oaks'
+let url = 'https://whispering-oaks.herokuapp.com/api/v1/whispering_oaks/'
 
 // if (process.env.NODE_ENV === 'development') {
 //   baseURL = 'http://localhost:5000/api/v1/whispering_oaks/';
